32ce52211e256370cf68feb3075a7c69789f972a
chollid <chrisuga@me.com>
2021-07-12 20:48:59 -0400

    New Screen

---
+++ src/screens/GithubStuff.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const GithubStuff = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default GithubStuff;
+
+const styles = StyleSheet.create({});
